4446afd7c0de6789ac2acd8148020c1ed91fe5b4
Omar-Codess <omartaki8@gmail.com>
2023-03-14 17:56:38 +0100

    creato file txt

--- src/org/lessons/java/exceptions/books/Main.java
+++ src/org/lessons/java/exceptions/books/Main.java
@@ -1,10 +1,16 @@
 package org.lessons.java.exceptions.books;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
+
+    private final static String FILE_PATH = "./books.txt";
+    public static void main (String[] args) throws IOException {
 
         Scanner scan = new Scanner(System.in);
 
@@ -35,6 +41,45 @@ public class Main {
                 e.printStackTrace();
             }
         }
+
+        scan.close();
         System.out.println(Arrays.toString(array));
+
+        FileWriter myWriter = null;
+        try {
+            myWriter = new FileWriter(FILE_PATH, true);
+            for (Book currentBook : array) {
+                myWriter.write(currentBook.toString() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Non posso creare il file");
+            System.out.println(e.getMessage());
+        } finally {
+            if (myWriter != null) {
+                try {
+                    myWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(new File(FILE_PATH));
+            while (fileScanner.hasNext()) {
+                String line = fileScanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (fileScanner != null) {
+                try {
+                    fileScanner.close();
+                } catch (IllegalStateException ise) {
+                    ise.printStackTrace();
+                }
+            }
+        }
     }
 }

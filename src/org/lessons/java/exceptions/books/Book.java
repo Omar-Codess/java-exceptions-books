41dd98644a764be94379b96a8f52b80680a79cb2
Omar-Codess <omartaki8@gmail.com>
2023-03-14 14:28:52 +0100

    istanziati attributi, costruttore, getter e setter

--- src/org/lessons/java/exceptions/books/Book.java
+++ src/org/lessons/java/exceptions/books/Book.java
@@ -1,5 +1,50 @@
 package org.lessons.java.exceptions.books;
 
 public class Book {
-    
+    //ATTRIBUTI
+    private String title;
+    private int pagesNum;
+    private String author;
+    private String editor;
+
+    //COSTRUTTORE
+    public Book(String title, int pagesNum, String author, String editor) {
+        this.title = title;
+        this.pagesNum = pagesNum;
+        this.author = author;
+        this.editor = editor;
+    }
+
+    //GETTER E SETTER
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPagesNum() {
+        return pagesNum;
+    }
+
+    public void setPagesNum(int pagesNum) {
+        this.pagesNum = pagesNum;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
 }
